19fd1a46a04c5eb8288bdc0bc5a971f12378459e
liana <lianarhcp@gmail.com>
2023-08-31 11:29:38 -0300

    ej 10 listo para entregar

--- ejercicio10.R
+++ ejercicio10.R
@@ -15,16 +15,18 @@ rm(list=ls())
 # carrera cuenta con 20 materias en total y que el alumno se inscribi´o despu´es de 1999. Antes de escribir
 # el c´odigo, escriba el diagrama de flujo asociado.
 
-# defino las variables que quiero que me ingresen por pantalla
+# defino las variables con los datos que quiero que me ingrese por pantalla el alumno
 NomAp <- readline("Ingrese su nombre y apellido:")
 lib_uni <- readline("Ingrese su numero de libreta en formato N/AA:")
 cant_mat_ap <- as.numeric(readline("Ingrese la cantidad de materias aprobados hasta el momento:"))
 
-# separo las variables antes definidas para poder armar el texto pedido 
+# redefino variables a partir de la separacion de las variables antes definidas para poder armar el texto pedido 
+# separo el NomAp en Nombre y Apellido
 sep_NomAp <- scan(text = NomAp, what = "")
 Nombre <- sep_NomAp[1]
 Apellido <- sep_NomAp[2]
 
+# separo la libreta universitaria en N y AA
 long_lib <- nchar(lib_uni)
 N <- substr(lib_uni, 1, long_lib-3)
 AA <- substr(lib_uni, long_lib-1, long_lib)
@@ -38,5 +40,3 @@ anio <- paste0("20", AA)
 
 texto <- print(paste("El alumno", Nombre, Apellido, "se inscribió como alumno de Exactas en el puesto", N, "en el año", anio, "y debe aprobar", X, "materias para obtener el título de grado"))
 
-
-# token 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
